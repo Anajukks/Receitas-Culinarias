a4d5cbdc81287d46265a87a70ee40e8c50781a10
francilaniaaraujo <138123064+FrancilaniaAraujo@users.noreply.github.com>
2024-11-26 14:06:18 -0300

    Update Contatenos.js

--- pagina-sobre/Contatenos.js
+++ pagina-sobre/Contatenos.js
@@ -5,44 +5,43 @@ import endereço from "./endereço.png";
 import insta from "./insta.png";
 import telefone from "./telefone.jpg"
 
-export default function Contatenos(){
-    return(
-        <div class="d-flex flex-column bd-highlight mb-3 justify-content-evenly">
-            <div id="tres" class="d-flex justify-content-center ">
-                <div class="p-1">
-                    <h1 class="fs-3">Contate-nos</h1>
+export default function Contatenos() {
+    return (
+        <div className="d-flex flex-column bd-highlight mb-3 justify-content-evenly">
+            <div id="tres" className="d-flex justify-content-center">
+                <div className="p-1">
+                    <h1 className="fs-3">Formas de Contato</h1>
                 </div>
             </div>
 
-            <div class="d-flex justify-content-evenly p-3 m-3">
-            <div id="quatro"class="d-flex flex-column  ">
-                    <div class="cinco">
-                        <img class="imagem"src={endereço}></img>
+            <div className="d-flex justify-content-evenly p-3 m-3">
+                <div id="quatro" className="d-flex flex-column align-items-center">
+                    <div className="cinco">
+                        <img className="imagem" src={endereço} alt="Localização" />
                     </div>
-                    <div class="cinco">
+                    <div className="cinco">
                         <h6>Localização</h6>
                     </div>
                 </div>
 
-                <div id="quatro" class="d-flex flex-column ">
-                    <div class="cinco">
-                        <img class="imagem" src={telefone}></img>
+                <div id="quatro" className="d-flex flex-column align-items-center">
+                    <div className="cinco">
+                        <img className="imagem" src={telefone} alt="Telefone" />
                     </div>
-                    <div class="cinco">
+                    <div className="cinco">
                         <h6>Telefone</h6>
                     </div>
                 </div>
 
-                <div id="quatro"class="d-flex flex-column ">
-                    <div class="cinco">
-                        <img class="imagem" src={insta}></img>
+                <div id="quatro" className="d-flex flex-column align-items-center">
+                    <div className="cinco">
+                        <img className="imagem" src={insta} alt="Instagram" />
                     </div>
-                    <div class="cinco">
+                    <div className="cinco">
                         <h6>Instagram</h6>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 }
