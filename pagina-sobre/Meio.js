7369734d45fcd9d90354eea6234cfa7ee229ce84
Ana Júlia <111990344+Anajukks@users.noreply.github.com>
2024-12-01 09:02:05 -0300

    Update Meio.js

--- pagina-sobre/Meio.js
+++ pagina-sobre/Meio.js
@@ -7,12 +7,17 @@ export default function Meio(){
     return(
         <div class='d-flex justify-content-evenly p-3 m-3'>
             <div class="dois">
-                <h6>A culinária desempenha um papel fundamental na vida humana, transcendendo a mera necessidade de se alimentar.
-                    <p>Ela é uma arte, uma ciência e uma expressão cultural que reflete a identidade e as tradições de diferentes sociedades ao redor do mundo.</p>
-                    <p>Do ponto de vista da saúde, a culinária também desempenha um papel crucial. A escolha dos ingredientes e a forma de preparo influenciam diretamente na qualidade da alimentação.</p>
-                    <p>Cozinhar em casa permite que as pessoas tenham maior controle sobre o que consomem, promovendo uma dieta mais equilibrada e saudável. </p>
-                    <p>Além disso, a culinária saudável pode ajudar na prevenção de doenças e na promoção do bem-estar.</p>
-                </h6>
+                <p class="descricao-projeto">
+                    A culinária desempenha um papel fundamental na vida humana, transcendendo 
+                    a mera necessidade de se alimentar. Ela é uma arte, uma ciência e uma expressão cultural 
+                    que reflete a identidade e as tradições de diferentes sociedades ao redor do mundo. 
+                    Do ponto de vista da saúde, a culinária também desempenha um papel crucial. A escolha 
+                    dos ingredientes e a forma de preparo influenciam diretamente na qualidade da alimentação. 
+                    Cozinhar em casa permite que as pessoas tenham maior controle sobre o que consomem, promovendo 
+                    uma dieta mais equilibrada e saudável. Além disso, a culinária saudável pode ajudar na prevenção 
+                    de doenças e na promoção do bem-estar.
+                </p>
+
             </div>
             <div class="um" >
             <img src={cozinhando}></img>
