9983f15ac4e578b2e316e84e9f17cfd98c06f140
Ana Júlia <111990344+Anajukks@users.noreply.github.com>
2024-12-01 08:04:12 -0300

    Update Rodape.js

--- Rodape.js
+++ Rodape.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import { FaInstagram, FaWhatsapp, FaFacebook } from 'react-icons/fa';
 import './Rodape.css';
-import logo from './logo_projeto.png'; // Substitua 'logo.png' pelo nome do arquivo da logo
+import logo from './logo_projeto.png'; 
 
 export default function Rodape() {
     return (
         <div className='rodape'>
             <div className='rodape-container'>
-                {/* Logo à esquerda */}
                 <div className='logo-section'>
                     <img src={logo} alt="Logo" className="logo" />
                 </div>
-
-                {/* Contatos no centro */}
                 <div className='contact-section'>
                     <h5>Contatos</h5>
                     <p>Email: cozinhaembytes@escolar.ifrn.edu.br</p>
                     <p>Telefone: (84) 99999-9999</p>
                 </div>
-
-                {/* Redes sociais à direita */}
                 <div className='social-section'>
                     <h5>Fique por dentro!</h5>
                     <div className='social-icons'>
